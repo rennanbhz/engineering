7174e8bb34bce7924c65bf725a90ab5ac4405e5c
Renan Mattos <rennanbhz@hotmail.com>
2019-03-19 11:09:06 -0300

    Project Started

---
+++ engineering-technical-test/src/main/java/com/engineering/engineeringtechnicaltest/EngineeringTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.engineering.engineeringtechnicaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineeringTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineeringTechnicalTestApplication.class, args);
+	}
+
+}
